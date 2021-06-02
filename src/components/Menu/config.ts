8150e6f41688f05d0c3cd661b46dffc8a40ac690
Coins Bitty <84514483+coinsbittytester@users.noreply.github.com>
2021-06-02 13:26:59 -0700

    g

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -11,12 +11,12 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
   {
     label: t('Exchange'),
     icon: 'TradeIcon',
-    href: 'exchange/#/swap?outputCurrency=0xaabcac4b236d1a7290fb05234862348d274621cf',
+    href: '/exchange/#/swap?outputCurrency=0xaabcac4b236d1a7290fb05234862348d274621cf',
   },
   {
     label: t('Liquidity'),
     icon: 'TradeIcon',
-    href: 'exchange/#/add/BNB/0xaabcac4b236d1a7290fb05234862348d274621cf',
+    href: '/exchange/#/add/BNB/0xaabcac4b236d1a7290fb05234862348d274621cf',
   },
   {
     label: t('Prediction (BETA)'),
